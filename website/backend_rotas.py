0db0aeb65e4b9460e3df0a7902cba58a56ac56b9
Kae Bartlett <kae@voxelfox.co.uk>
2022-12-03 23:29:59 +0000

    Fix return value of putting the rota values

--- website/backend_rotas.py
+++ website/backend_rotas.py
@@ -90,6 +90,8 @@ async def api_post_rotas(request: Request):
                         $1,
                         $2
                     )
+                RETURNING 
+                    *
                 """,
                 login_id, data['availability'],
             )
@@ -101,60 +103,10 @@ async def api_post_rotas(request: Request):
                 status=400,
             )
 
-    # Return the venues
+    # Don't return anything - the page refreshes out of ease
     return json_response(
         {
-            "data": utils.encode_row_as_json(rotas[0])
-        },
-        status=200,
-    )
-
-
-@routes.delete("/api/venues")
-@utils.requires_login(api_response=True)
-async def api_delete_venue(request: Request):
-    """
-    Delete a venue from the user.
-    """
-
-    # Validate the request
-    if (r := utils.ensure_required_keys(request.query, {"id"}, location="query")):
-        return r
-    if (r := utils.check_valid_uuid(request.query["id"], api_response=True)):
-        return r
-
-    # Get the user's ID
-    session = await aiohttp_session.get_session(request)
-    login_id = session.get("id")
-    assert login_id, "Missing login ID"
-
-    # Delete the venue
-    async with vbu.Database() as db:
-        venue_rows = await db.call(
-            """
-            DELETE FROM
-                venues
-            WHERE
-                owner_id = $1
-            AND
-                id = $2
-            RETURNING
-                *
-            """,
-            login_id, request.query["id"],
-        )
-
-    # Return if we've been successful
-    if not venue_rows:
-        return json_response(
-            {
-                "message": "Venue not found.",
-            },
-            status=404,
-        )
-    return json_response(
-        {
-            "message": "Venue deleted.",
+            "data": {}
         },
         status=200,
     )
